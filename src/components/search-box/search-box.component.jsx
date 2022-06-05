f21bba2453a983f02672b3d390ffedb75b7ccde7
Leonid Baidulin <lbajdull@gmail.com>
2022-06-05 15:35:54 +0300

    add full project code

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,35 @@
+// = = = Hooks = = =
+import React from 'react';
+
+import './search-box.styles.css';
+
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    className={`search-box ${className}`}
+    type='search'
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+export default SearchBox;
+
+// = = = Class = = =
+// import { Component } from 'react';
+
+// import './search-box.styles.css'
+
+// class SearchBox extends Component {
+//   render() {
+//     return (
+//       <input
+//         className={`search-box ${this.props.className}`}
+//         type='search'
+//         placeholder={this.props.placeholder}
+//         onChange={this.props.onChangeHandler}
+//       />
+//     );
+//   }
+// }
+
+// export default SearchBox;
